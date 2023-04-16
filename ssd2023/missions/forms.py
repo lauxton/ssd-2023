6829e5d45641350e6771bdcd6aedf08e34be02a4
Xue Ling Teh <107624269+xcelt@users.noreply.github.com>
2023-04-16 18:32:37 +1000

    Add CRUD missions and generate reports

--- ssd2023/missions/forms.py
+++ ssd2023/missions/forms.py
@@ -1,1 +1,52 @@
-# TODO: add more forms
+from django import forms
+from django.forms import Form, ModelForm, TextInput, DateTimeInput, Select
+
+from .models import Mission, Employee
+
+
+class MissionForm(ModelForm):
+    """Form for creating a new mission."""
+
+    class Meta:
+        model = Mission
+        fields = [
+            'name',
+            'description',
+            'division',
+            'supervisor',
+            'start_date',
+            'end_date',
+            'security_clearance',
+        ]
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control'}),
+            'description': TextInput(attrs={'class': 'form-control'}),
+            'division': Select(attrs={'class': 'form-control'}),
+            'supervisor': Select(attrs={'class': 'form-control'}),
+            'start_date': DateTimeInput(attrs={'class': 'form-control'}),
+            'end_date': DateTimeInput(attrs={'class': 'form-control'}),
+            'security_clearance': Select(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'name': 'Mission Name',
+            'description': 'Mission Description',
+            'division': 'Division',
+            'supervisor': 'Supervisor',
+            'start_date': 'Start Date',
+            'end_date': 'End Date',
+            'security_clearance': 'Security Clearance',
+        }
+
+
+class GenerateReportForm(Form):
+    """Form for generating a new report for an existing mission."""
+
+    assigned_to = forms.ModelChoiceField(
+        queryset=Employee.objects.filter(user__groups__name='NASA_Admin_User'),
+        required=True,
+        widget=Select(attrs={'class': 'form-control'})
+    )
+    report_summary = forms.CharField(
+        required=True,
+        widget=TextInput(attrs={'class': 'form-control'})
+    )
